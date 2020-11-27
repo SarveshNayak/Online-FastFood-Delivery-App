b93dc4f3d8900dd91e73df8ef52173927fdb6067
SarveshNayak <sarveshnayak1998@gmail.com>
2020-11-27 15:16:22 +0530

    added some coment

--- lib/database_helper.dart
+++ lib/database_helper.dart
@@ -83,6 +83,7 @@ class DatabaseHelper {
     return result;
   }
 
+//get the 'map list' [List<Map>] and convert it to 'Note List' [List<Note>]
   Future<List<Note>> getNoteList() async {
     var noteMapList = await getNoteMapList();
     int count = noteMapList.length;
