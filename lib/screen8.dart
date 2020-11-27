a7e6a2856ed51a24146c5857f9fe799513dca61e
SarveshNayak <sarveshnayak1998@gmail.com>
2020-11-27 10:42:59 +0530

    done the argument error

--- lib/screen8.dart
+++ lib/screen8.dart
@@ -658,11 +658,12 @@ class Screen8a extends State<Screen8> {
                   ),
                   onPressed: () {
                     setState(() {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => Screen9(),
-                          ));
+                      String user = "sarvesh";
+                      int age = 20;
+                      Navigator.push(context,
+                          new MaterialPageRoute(builder: (context) {
+                        return Screen9(user, age);
+                      }));
                     });
                   }),
             ),
