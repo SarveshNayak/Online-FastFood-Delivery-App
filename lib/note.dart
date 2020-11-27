295f2363f91f4383e8bf128947717da670e19caa
SarveshNayak <sarveshnayak1998@gmail.com>
2020-11-27 11:30:16 +0530

    changed if condition in line 36

--- lib/note.dart
+++ lib/note.dart
@@ -1,48 +1,50 @@
-class Note{
-
+class Note {
   String _item;
   int _quantity;
   int _rate;
-  
-  Note(this._item,this._quantity,this._rate);
 
-  Note.one(this._item,this._quantity,this._rate);
+  Note(this._item, this._quantity, this._rate);
 
-  String get item =>_item;
-  int get quantity =>_quantity;
-  int get rate =>_rate;
+  Note.one(this._item, this._quantity, this._rate);
 
-  set item(String newItem){
-    if(1==1){
+  String get item => _item;
+
+  int get quantity => _quantity;
+
+  int get rate => _rate;
+
+  set item(String newItem) {
+    if (newItem.length <= 100) {
       this._item = newItem;
     }
   }
 
-  set quantity(int newQuantity){
-    if(newQuantity<=10000){
-      this._quantity=newQuantity;
+  set quantity(int newQuantity) {
+    if (newQuantity <= 10000) {
+      this._quantity = newQuantity;
     }
   }
 
-  set rate(int newRate){
-    if(newRate<=10000){
-      this._rate=newRate;
+  set rate(int newRate) {
+    if (newRate <= 10000) {
+      this._rate = newRate;
     }
   }
 
-  Map<String,dynamic>toMap(){
+  Map<String, dynamic> toMap() {
+    var map = Map<String, dynamic>();
+    if (item != null) {
+      map['item'] = _item;
+    }
 
-    var map=Map<String,dynamic>();
-    map['item']=_item;
-    map['quantity']=_quantity;
-    map['rate']=_rate;
+    map['quantity'] = _quantity;
+    map['rate'] = _rate;
     return map;
   }
-  
-  Note.fromMapObject(Map<String,dynamic>map){
-    this.item=map['item'];
-    this.quantity=map['quantity'];
-    this.rate=map['rate'];
-  }
 
+  Note.fromMapObject(Map<String, dynamic> map) {
+    this.item = map['item'];
+    this.quantity = map['quantity'];
+    this.rate = map['rate'];
+  }
 }
