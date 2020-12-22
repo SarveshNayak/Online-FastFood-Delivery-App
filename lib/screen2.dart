741fc999ed1d69dfa6e3257de87730890e8703ea
SarveshNayak <sarveshnayak1998@gmail.com>
2020-12-22 12:04:59 +0530

    changed the ui

--- lib/screen2.dart
+++ lib/screen2.dart
@@ -14,6 +14,11 @@ class Screen2 extends StatefulWidget {
 }
 
 class Screen2a extends State<Screen2> {
+  double gap = 0.07;
+  double btgap = 0.03;
+  double conwidth = 0.9;
+  double conheight = 0.12;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,195 +30,186 @@ class Screen2a extends State<Screen2> {
             )),
             child: Container(
                 child: Column(children: [
-              Container(height: MediaQuery.of(context).size.height * 0.08),
-              Row(children: [
-                Container(width: MediaQuery.of(context).size.width * 0.03),
-                Container(
-                  width: MediaQuery.of(context).size.height * 0.22,
-                  height: MediaQuery.of(context).size.height * 0.22,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(30.00),
-                      border: Border.all(width: 2, color: Colors.white),
-                      image: DecorationImage(
-                        image: AssetImage('images/fries.jpg'),
-                        fit: BoxFit.cover,
-                      )),
-                  child: RaisedButton(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(30.00),
-                      ),
-                      color: Colors.transparent,
-                      child: Text(
-                        "Fries",
-                        style: TextStyle(fontSize: 27.00, color: Colors.white),
-                      ),
-                      onPressed: () {
-                        setState(() {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => Screen3(),
-                              ));
-                        });
-                      }),
-                ),
-                Container(width: MediaQuery.of(context).size.width * 0.03),
-                Container(
-                  width: MediaQuery.of(context).size.height * 0.22,
-                  height: MediaQuery.of(context).size.height * 0.22,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(30.00),
-                      border: Border.all(width: 2, color: Colors.white),
-                      image: DecorationImage(
-                        image: AssetImage('images/burger.jpg'),
-                        fit: BoxFit.cover,
-                      )),
-                  child: RaisedButton(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(30.00),
-                      ),
-                      color: Colors.transparent,
-                      child: Text(
-                        "Burger",
-                        style: TextStyle(fontSize: 27.00, color: Colors.white),
-                      ),
-                      onPressed: () {
-                        setState(() {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => Screen4(),
-                              ));
-                        });
-                      }),
-                ),
-              ]),
-              Container(height: MediaQuery.of(context).size.height * 0.08),
-              Row(children: [
-                Container(width: MediaQuery.of(context).size.width * 0.03),
-                Container(
-                  width: MediaQuery.of(context).size.height * 0.22,
-                  height: MediaQuery.of(context).size.height * 0.22,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(30.00),
-                      border: Border.all(width: 2, color: Colors.white),
-                      image: DecorationImage(
-                        image: AssetImage('images/pasta1.jpg'),
-                        fit: BoxFit.cover,
-                      )),
-                  child: RaisedButton(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(30.00),
-                      ),
-                      color: Colors.transparent,
-                      child: Text(
-                        "Pasta",
-                        style: TextStyle(fontSize: 27.00, color: Colors.white),
-                      ),
-                      onPressed: () {
-                        setState(() {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => Screen5(),
-                              ));
-                        });
-                      }),
-                ),
-                Container(width: MediaQuery.of(context).size.width * 0.03),
-                Container(
-                  width: MediaQuery.of(context).size.height * 0.22,
-                  height: MediaQuery.of(context).size.height * 0.22,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(30.00),
-                      border: Border.all(width: 2, color: Colors.white),
-                      image: DecorationImage(
-                        image: AssetImage('images/sandwich3.jpg'),
-                        fit: BoxFit.cover,
-                      )),
-                  child: RaisedButton(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(30.00),
-                      ),
-                      color: Colors.transparent,
-                      child: Text(
-                        "Sandwich",
-                        style: TextStyle(fontSize: 27.00, color: Colors.white),
-                      ),
-                      onPressed: () {
-                        setState(() {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => Screen6(),
-                              ));
-                        });
-                      }),
-                ),
-              ]),
-              Container(height: MediaQuery.of(context).size.height * 0.08),
-              Row(children: [
-                Container(width: MediaQuery.of(context).size.width * 0.03),
-                Container(
-                  width: MediaQuery.of(context).size.height * 0.22,
-                  height: MediaQuery.of(context).size.height * 0.22,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(30.00),
-                      border: Border.all(width: 2, color: Colors.white),
-                      image: DecorationImage(
-                        image: AssetImage('images/nachos.jpg'),
-                        fit: BoxFit.cover,
-                      )),
-                  child: RaisedButton(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(30.00),
-                      ),
-                      color: Colors.transparent,
-                      child: Text(
-                        "Nachos",
-                        style: TextStyle(fontSize: 27.00, color: Colors.white),
-                      ),
-                      onPressed: () {
-                        setState(() {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => Screen7(),
-                              ));
-                        });
-                      }),
-                ),
-                Container(width: MediaQuery.of(context).size.width * 0.03),
-                Container(
-                  width: MediaQuery.of(context).size.height * 0.22,
-                  height: MediaQuery.of(context).size.height * 0.22,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(30.00),
-                      border: Border.all(width: 2, color: Colors.white),
-                      image: DecorationImage(
-                        image: AssetImage('images/frankie.JPG'),
-                        fit: BoxFit.cover,
-                      )),
-                  child: RaisedButton(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(30.00),
-                      ),
-                      color: Colors.transparent,
-                      child: Text(
-                        "Frankies",
-                        style: TextStyle(fontSize: 27.00, color: Colors.white),
-                      ),
-                      onPressed: () {
-                        setState(() {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => Screen8(),
-                              ));
-                        });
-                      }),
-                ),
-              ])
+              Container(height: MediaQuery.of(context).size.height * gap),
+              Container(
+                width: MediaQuery.of(context).size.width * conwidth,
+                height: MediaQuery.of(context).size.height * conheight,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20.00),
+                    border: Border.all(width: 2, color: Colors.white),
+                    image: DecorationImage(
+                      image: AssetImage('images/fries.jpg'),
+                      fit: BoxFit.cover,
+                    )),
+                child: RaisedButton(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20.00),
+                    ),
+                    color: Colors.transparent,
+                    child: Text(
+                      "Fries",
+                      style: TextStyle(fontSize: 45.00, color: Colors.white),
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => Screen3(),
+                            ));
+                      });
+                    }),
+              ),
+              Container(height: MediaQuery.of(context).size.height * btgap),
+              Container(
+                width: MediaQuery.of(context).size.width * conwidth,
+                height: MediaQuery.of(context).size.height * conheight,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20.00),
+                    border: Border.all(width: 2, color: Colors.white),
+                    image: DecorationImage(
+                      image: AssetImage('images/burger.jpg'),
+                      fit: BoxFit.cover,
+                    )),
+                child: RaisedButton(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20.00),
+                    ),
+                    color: Colors.transparent,
+                    child: Text(
+                      "Burger",
+                      style: TextStyle(fontSize: 45.00, color: Colors.white),
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => Screen4(),
+                            ));
+                      });
+                    }),
+              ),
+              Container(height: MediaQuery.of(context).size.height * btgap),
+              Container(
+                width: MediaQuery.of(context).size.width * conwidth,
+                height: MediaQuery.of(context).size.height * conheight,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20.00),
+                    border: Border.all(width: 2, color: Colors.white),
+                    image: DecorationImage(
+                      image: AssetImage('images/pasta1.jpg'),
+                      fit: BoxFit.cover,
+                    )),
+                child: RaisedButton(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20.00),
+                    ),
+                    color: Colors.transparent,
+                    child: Text(
+                      "Pasta",
+                      style: TextStyle(fontSize: 45.00, color: Colors.white),
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => Screen5(),
+                            ));
+                      });
+                    }),
+              ),
+              Container(height: MediaQuery.of(context).size.height * btgap),
+              Container(
+                width: MediaQuery.of(context).size.width * conwidth,
+                height: MediaQuery.of(context).size.height * conheight,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20.00),
+                    border: Border.all(width: 2, color: Colors.white),
+                    image: DecorationImage(
+                      image: AssetImage('images/sandwich3.jpg'),
+                      fit: BoxFit.cover,
+                    )),
+                child: RaisedButton(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20.00),
+                    ),
+                    color: Colors.transparent,
+                    child: Text(
+                      "Sandwich",
+                      style: TextStyle(fontSize: 45.00, color: Colors.white),
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => Screen6(),
+                            ));
+                      });
+                    }),
+              ),
+              Container(height: MediaQuery.of(context).size.height * btgap),
+              Container(
+                width: MediaQuery.of(context).size.width * conwidth,
+                height: MediaQuery.of(context).size.height * conheight,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20.00),
+                    border: Border.all(width: 2, color: Colors.white),
+                    image: DecorationImage(
+                      image: AssetImage('images/nachos.jpg'),
+                      fit: BoxFit.cover,
+                    )),
+                child: RaisedButton(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20.00),
+                    ),
+                    color: Colors.transparent,
+                    child: Text(
+                      "Nachos",
+                      style: TextStyle(fontSize: 45.00, color: Colors.white),
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => Screen7(),
+                            ));
+                      });
+                    }),
+              ),
+              Container(height: MediaQuery.of(context).size.height * btgap),
+              Container(
+                width: MediaQuery.of(context).size.width * conwidth,
+                height: MediaQuery.of(context).size.height * conheight,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20.00),
+                    border: Border.all(width: 2, color: Colors.white),
+                    image: DecorationImage(
+                      image: AssetImage('images/frankie.JPG'),
+                      fit: BoxFit.cover,
+                    )),
+                child: RaisedButton(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20.00),
+                    ),
+                    color: Colors.transparent,
+                    child: Text(
+                      "Frankies",
+                      style: TextStyle(fontSize: 45.00, color: Colors.white),
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => Screen8(),
+                            ));
+                      });
+                    }),
+              ),
             ]))));
   }
 }
